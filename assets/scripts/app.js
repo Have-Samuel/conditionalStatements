72f44593f6e01567a5d3e1d53647494c4fd3c062
Have Samuel <samiehave9@gmail.com>
2022-12-13 07:18:16 +0300

    Utiliziing Global constants in conditional code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,9 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
+const MODE_ATTACK = 'ATTACK'; // MODE_ATTACK = 0
+const MODE_STRONG_ATTACK = 'STRONG_ATTACK';// MODE_STRONG_ATTACK = 1
+
 const enteredNumber = prompt('Maximum Life for you and the Monster.', '100');
 
 let chosenMaxLife = parseInt(enteredNumber);
@@ -64,9 +67,9 @@ function endRound() {
 
 function monsterAttack(mode) {
   let maxDamage;
-  if (mode === 'ATTACK') {
+  if (mode === MODE_ATTACK) {
     maxDamage = ATTACK_VALUE;
-  } else if (mode === 'STRONG_ATTACK') {
+  } else if (mode === MODE_STRONG_ATTACK) {
     maxDamage = STRONG_ATTACK_VALUE;
   }
   const damage = dealMonsterDamage(maxDamage); // monster attack
@@ -75,11 +78,11 @@ function monsterAttack(mode) {
 }
 
 function attackHandler() {
-  monsterAttack('ATTACK');
+  monsterAttack(MODE_ATTACK);
 }
 
 function strongAttackHandler() {
-  monsterAttack('STRONG_ATTACK');
+  monsterAttack(MODE_STRONG_ATTACK);
 }
 
 function healPlayerHandler() {
